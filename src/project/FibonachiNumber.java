22b61cf66a065a3387641a9a7c1dc05cd9c5c7ce
Кирило <kyrylo.kyrychenko.oi.2024@lpnu.ua>
2025-10-20 21:23:54 +0300

    Лабораторна 1

--- src/project/FibonachiNumber.java
+++ src/project/FibonachiNumber.java
@@ -33,14 +33,9 @@ public class FibonachiNumber {
      @return true, якщо число = k² + 1
      */
         public boolean isSquarePlusOne() {
-            if (value - 1 <= 0) return false;
-
-            for (long k = 1; k * k <= value - 1; k++) {
-                if (k * k == value - 1) {
-                    return true;
-                }
-            }
-            return false;
+            if (value <= 1) return false;
+            long k = (long) Math.pow(value, 0.5);
+            return k*k == value-1 ;
         }
 
         public String info() {
--- src/project/Main.java
+++ src/project/Main.java
@@ -13,6 +13,59 @@ import java.util.Scanner;
  */
 public class Main {
 
+    public static int readn (String[] args) {
+        Scanner scan = new Scanner(System.in);
+            int n;
+
+            if (args.length > 0) {
+                n = Integer.parseInt(args[0]);
+                System.out.println("Введено через командний рядок: N = " + n);
+            } else {
+                System.out.print("Введіть кількість чисел Фібоначчі N (Не більше 80): ");
+                n = scan.nextInt();
+            }
+            return n;
+    }
+
+
+
+    public static FibonachiNumber[] createFibonachi (int n) {
+
+        FibonachiNumber[] numbers = new FibonachiNumber[n];
+
+            System.out.println("\nПерші " + n + " чисел Фібоначчі:");
+            for (int i = 1; i <= n; i++) {
+                long value = fibonacci(i);
+                numbers[i - 1] = new FibonachiNumber(i, value);
+                System.out.println(numbers[i - 1].info());
+            }
+            return numbers;
+    }
+
+    /*
+         Виводить числа фібоначі які можна подати як k^2 + 1.
+
+         @param FibonachiNumber[]  numbers числа фібоначі до числа з порядковим номером n
+         @param n порядковий номер числа (n ≥ 1)
+     */
+    public static void printresults (FibonachiNumber[]  numbers, int n) {
+        System.out.println("\nЧисла Фібоначчі, які можна подати як k^2 + 1:");
+        boolean found = false;
+        for (FibonachiNumber num : numbers) {
+            if (num.isSquarePlusOne()) {
+                System.out.println(num.info() + " = " + ((long)Math.pow(num.getValue()-1, 0.5)) + "^2 + 1");
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Серед перших " + n + " чисел таких немає.");
+        }
+
+    }
+
+
+
     /*
      Обчислює n-те число Фібоначчі.
 
@@ -36,46 +89,19 @@ public class Main {
 
      <p>Можливості:</p>
      <ul>
-       <li>Приймає число N через аргументи командного рядка.</li>
-       <li>Якщо аргумент не задано – запитує N через {@link Scanner}.</li>
-       <li>Обчислює перші N чисел Фібоначчі та виводить їх.</li>
+       <li>Приймає число n через аргументи командного рядка.</li>
+       <li>Якщо аргумент не задано – запитує n через {@link Scanner}.</li>
+       <li>Обчислює перші n чисел Фібоначчі та виводить їх.</li>
        <li>Перевіряє, які з них можна подати як k² + 1.</li>
      </ul>
 
-     @param args аргументи командного рядка; якщо args[0] задано – це число N
+     @param args аргументи командного рядка; якщо args[0] задано – це число n
      */
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N;
 
-        if (args.length > 0) {
-            N = Integer.parseInt(args[0]);
-            System.out.println("Введено через командний рядок: N = " + N);
-        } else {
-            System.out.print("Введіть кількість чисел Фібоначчі N (Не більше 80): ");
-            N = scan.nextInt();
-        }
+        int n= readn(args);
+        FibonachiNumber[] numbers = createFibonachi(n);
+        printresults(numbers, n);
 
-        FibonachiNumber[] numbers = new FibonachiNumber[N];
-
-        System.out.println("\nПерші " + N + " чисел Фібоначчі:");
-        for (int i = 1; i <= N; i++) {
-            long value = fibonacci(i);
-            numbers[i - 1] = new FibonachiNumber(i, value);
-            System.out.println(numbers[i - 1].info());
-        }
-
-        System.out.println("\nЧисла Фібоначчі, які можна подати як k^2 + 1:");
-        boolean found = false;
-        for (FibonachiNumber num : numbers) {
-            if (num.isSquarePlusOne()) {
-                System.out.println(num.info() + " = " + ((long)Math.pow(num.getValue()-1, 0.5)) + "^2 + 1");
-                found = true;
-            }
-        }
-
-        if (!found) {
-            System.out.println("Серед перших " + N + " чисел таких немає.");
-        }
     }
 }
